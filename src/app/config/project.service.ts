f271becc2f729903b93bff060272ee8f7f82f600
Ettore Rollo <ettorerollo@hotmail.com>
2022-10-09 21:56:17 -0300

    Adicionado pasta config com service de urls das apis

---
+++ src/app/config/project.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProjectService { }
+
+export const urlDatabase = "http://localhost:3000";
+export const urlCep = "https://viacep.com.br/ws/";
