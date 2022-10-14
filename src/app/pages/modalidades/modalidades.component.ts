519f00101297c7258f2c075d77e8c0d515f2a8d1
Ettore Rollo <ettorerollo@hotmail.com>
2022-10-14 16:13:50 -0300

    Tela inicial de modalidade desenvolvida

--- src/app/pages/modalidades/modalidades.component.ts
+++ src/app/pages/modalidades/modalidades.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ModalidadesService } from '../../services/modalidades.service';
 
 @Component({
   selector: 'app-modalidades',
   templateUrl: './modalidades.component.html',
-  styleUrls: ['./modalidades.component.css']
+  styleUrls: ['./modalidades.component.css', '../../shared/styles.css']
 })
 export class ModalidadesComponent implements OnInit {
 
-  constructor(private _auth: AuthenticationService, private _router: Router) { }
+  modalidades: any[] = [];
+
+  constructor(private _auth: AuthenticationService, private _router: Router, private _modalidades: ModalidadesService) { }
 
   ngOnInit(): void {
     if(!this._auth.authenticate()) {
       this._router.navigateByUrl('/login');
     }
+
+    this.buscarModalidades();
+  }
+
+  editar(id: number): void {
+    this._router.navigateByUrl('/clientes/editar/' + id);
+  }
+
+  excluir(id: number): void {
+    this._modalidades.deleteModalidade(id).subscribe(() => {
+      this.buscarModalidades();
+    });
+  }
+
+  buscarModalidades(): void {
+    this._modalidades.getAll().subscribe((data: any[]) => {
+      this.modalidades = data;
+    });
   }
 
 }
