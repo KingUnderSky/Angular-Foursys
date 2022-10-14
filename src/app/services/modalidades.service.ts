44b59d79926981b86751a4a1b4c91e7816211663
Ettore Rollo <ettorerollo@hotmail.com>
2022-10-14 16:07:33 -0300

    implementado crud no service de modalidades

--- src/app/services/modalidades.service.ts
+++ src/app/services/modalidades.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { config } from '../config/config';
+import { Modalidade } from '../models/modalidade';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,21 @@ export class ModalidadesService {
 
   getAll(): Observable<any> {
     return this._http.get(config.urlAPI + 'modalidades');
+  }
+
+  getOne(id: number): Observable<any> {
+    return this._http.get(config.urlAPI + 'modalidades/' + id);
+  }
+
+  postModalidade(obj: Modalidade): Observable<any> {
+    return this._http.post(config.urlAPI + 'modalidades', obj);
+  }
+
+  putModalidade(obj: Modalidade, id: number): Observable<any> {
+    return this._http.put(config.urlAPI + 'modalidades/' + id, obj);
+  }
+
+  deleteModalidade(id: number): Observable<any> {
+    return this._http.delete(config.urlAPI + 'modalidades/' + id);
   }
 }
