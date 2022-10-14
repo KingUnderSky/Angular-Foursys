2f45aed934f8bbdd6df4f39a62e7648e39535cfe
Ettore Rollo <ettorerollo@hotmail.com>
2022-10-14 14:21:43 -0300

    crud implementado no service de usuário

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { config } from '../config/config';
+import { Usuario } from '../models/usuario';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,22 @@ export class UsuariosService {
   constructor(private _http: HttpClient) { }
 
   getAll(): Observable<any> {
-    return this._http.get(config.urlAPI + 'usuarios');
+    return this._http.get(config.urlAPI + 'usuarios/');
+  }
+
+  getOne(id: number): Observable<any> {
+    return this._http.get(config.urlAPI + 'usuarios/' + id);
+  }
+
+  postUsuario(obj: Usuario): Observable<any> {
+    return this._http.post(config.urlAPI + 'usuarios/', obj);
+  }
+
+  putUsuario(obj: Usuario, id: number): Observable<any> {
+    return this._http.put(config.urlAPI + 'usuarios/' + id, obj);
+  }
+
+  deleteUsuario(id: number): Observable<any> {
+    return this._http.delete(config.urlAPI + 'usuarios/' + id);
   }
 }
